a9b3208f4896fce616b6f315bd73a19519c5572e
JolantaWro <jolanta.woronowska@gmail.com>
2024-05-28 07:43:38 +0200

    refactor code

--- Beginning/Function/take_and_check.py
+++ Beginning/Function/take_and_check.py
@@ -1,10 +1,9 @@
 def get_data():
-    user_answer = input("Enter number: ")
+    user_answer = input("Enter word: ")
     return take_return_type(user_answer)
 
 def take_return_type(user_info):
-    result = int(user_info)
-    return type(result).__name__
+    return type(user_info).__name__
 
 
 get_data()
