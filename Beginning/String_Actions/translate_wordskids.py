c7a08a75959434a3708becbc7445a40f0e9c4bad
JolantaWro <jolanta.woronowska@gmail.com>
2024-06-11 13:21:19 +0200

    word fix witch ay

--- Beginning/String_Actions/translate_wordskids.py
+++ Beginning/String_Actions/translate_wordskids.py
@@ -1,5 +1,11 @@
 def translate(text):
-    vowels = ["a", "e", "i", "o", "u"]
+    text = text.lower()
+    vowels = ["a", "e", "i", "o", "u", "xr", "yt"]
+    if text[0] in vowels or text[:2] in vowels:
+        return text + "ay"
+
     
-    return text
+print(translate("Apple"))
+print(translate("xray"))
+print(translate("yttria"))
 
