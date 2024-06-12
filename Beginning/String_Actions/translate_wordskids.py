f6c68f2cd864aa48202a3bc780be6494a7e8556d
JolantaWro <jolanta.woronowska@gmail.com>
2024-06-12 20:07:55 +0200

    refactor task

--- Beginning/String_Actions/translate_wordskids.py
+++ Beginning/String_Actions/translate_wordskids.py
@@ -13,18 +13,6 @@ For example:
 "pig" -> "igp" -> "igpay" (starts with single consonant)
 "chair" -> "airch" -> "airchay" (starts with multiple consonants)
 "thrush" -> "ushthr" -> "ushthray" (starts with multiple consonants)
-
-Rule 3
-If a word starts with zero or more consonants followed by "qu", first move those consonants (if any) and the "qu" part to the end of the word, and then add an "ay" sound to the end of the word.
-For example:
-"quick" -> "ickqu" -> "ay" (starts with "qu", no preceding consonants)
-"square" -> "aresqu" -> "aresquay" (starts with one consonant followed by "qu")
-
-Rule 4
-If a word starts with one or more consonants followed by "y", first move the consonants preceding the "y"to the end of the word, and then add an "ay" sound to the end of the word.
-Some examples:
-"my" -> "ym" -> "ymay" (starts with single consonant followed by "y")
-"rhythm" -> "ythmrh" -> "ythmrhay" (starts with multiple consonants followed by "y")
 """
 
 
@@ -43,9 +31,4 @@ def translate(text):
     elif text[:3] in consonant:
         return text[3:] + text[0:3] + "ay"
     
-# print(translate("Apple"))
-# print(translate("xray"))
-# print(translate("yttria"))
-# print(translate("pig"))
-# print(translate("chair"))
-# print(translate("thrush"))
+
