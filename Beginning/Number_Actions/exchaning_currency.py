038d25c9a7cac0e1bc7dba695a3087815e7cc2b3
JolantaWro <jolanta.woronowska@gmail.com>
2024-06-21 12:19:19 +0200

    add function get change

--- Beginning/Number_Actions/exchaning_currency.py
+++ Beginning/Number_Actions/exchaning_currency.py
@@ -7,3 +7,15 @@ def exchange_money(budget, exchange_rate):
     """
 
     return budget / exchange_rate
+
+def get_change(budget, exchanging_value):
+    """
+
+    :param budget: float - amount of money you own.
+    :param exchanging_value: float - amount of your money you want to exchange now.
+    :return: float - amount left of your starting currency after exchanging.
+    """
+
+    return budget - exchanging_value
+
+print(get_change(463000, 5000))
