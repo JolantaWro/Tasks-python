0c2558952c6776eb9328d392ac130e1eb49f0720
JolantaWro <jolanta.woronowska@gmail.com>
2024-08-19 21:24:19 +0200

    create steps function

--- Beginning/Lists/card_game.py
+++ Beginning/Lists/card_game.py
@@ -322,3 +322,17 @@ def is_leap_year(year):
             return True  # Divisible by 4, but not 100
     else:
         return False  # Not divisible by 4
+
+def steps(n):
+    if n <= 0:
+        raise ValueError("Only positive integers are allowed")
+    
+    steps = 0
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+        steps += 1
+    
+    return steps
