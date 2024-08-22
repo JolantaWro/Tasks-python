16c8efd3e8577b908604627c1f46fb288c14bff8
JolantaWro <jolanta.woronowska@gmail.com>
2024-08-22 14:40:41 +0200

    create function

--- Beginning/Lists/card_game.py
+++ Beginning/Lists/card_game.py
@@ -373,3 +373,20 @@ def convert(number):
         result = str(number)
     
     return result
+
+    
+def is_triangle(sides):
+    a, b, c = sorted(sides)
+    return a > 0 and a + b >= c
+
+
+def equilateral(sides):
+    return is_triangle(sides) and sides[0] == sides[1] == sides[2]
+
+
+def isosceles(sides):
+    return is_triangle(sides) and (sides[0] == sides[1] or sides[1] == sides[2] or sides[0] == sides[2])
+
+
+def scalene(sides):
+    return is_triangle(sides) and sides[0] != sides[1] != sides[2] != sides[0]
