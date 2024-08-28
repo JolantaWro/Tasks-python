cbe5d3fd0e7e5e06a0f10f0decc101d3370e7043
JolantaWro <jolanta.woronowska@gmail.com>
2024-08-28 13:28:07 +0200

    change function guessing and user input

--- Learning_program/Module_2/user_input.py
+++ Learning_program/Module_2/user_input.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 print('Welcome to the age calculator.')
-answer = input('Would you like to know how old you will be in a particular year? (y/n) ')
+answer = input('Would you like to know how old you will be in a particular year? (y/n)')
 
 while not (answer == 'y' or answer == 'n'):
-    print("The value of answer is ", answer)
-    print("I'm sorry, I didn't understand. Please try again. ")
-    answer = input('Would you like to know how old you will be in a particular year? (y/n) ')
+    print("The value of answer is", answer)
+    print("I'm sorry, I didn't understand. Please try again.")
+    answer = input('Would you like to know how old you will be in a particular year? (y/n)')
 
 if answer == 'n':
     print("I'm sorry you're not interested. Goodbye!")
@@ -19,61 +19,94 @@ else:
     futureMonth = input('What month did you want to know about? ')
     futureDay = input('What day? ')
 
-    if month.isdigit() is False:
-        if month == 'jan' or month == 'january' or month == 'Jan' or month == 'January':
+    jan = ['jan', 'january', 'Jan', 'January']
+    feb = ['feb', 'february', 'Feb', 'February']
+    mar = ['mar', 'march', 'Mar', 'March']
+    apr = ['apr', 'april', 'Apr', 'April']
+    may = ['may', 'May']
+    jun = ['jun', 'june', 'Jun', 'June']
+    jul = ['jul', 'july', 'Jul', 'July']
+    aug = ['aug', 'august', 'Aug', 'August']
+    sep = ['sep', 'september', 'Sep', 'September']
+    oct = ['oct', 'october', 'Oct', 'October']
+    nov = ['nov', 'november', 'Nov', 'November']
+    dec = ['dec', 'december', 'Dec', 'December']
+
+    for i in jan:
+        if month == i:
             month = 1
-        elif month == 'feb' or month == 'february' or month == 'Feb' or month == 'February':
-            month = 2
-        elif month == 'mar' or month == 'march' or month == 'Mar' or month == 'March':
-            month = 3
-        elif month == 'apr' or month == 'april' or month == 'Apr' or month == 'April':
-            month = 4
-        elif month == 'may' or month == 'May':
-            month = 5
-        elif month == 'jun' or month == 'june' or month == 'Jun' or month == 'February':
-            month = 6
-        elif month == 'jul' or month == 'july' or month == 'Jul' or month == 'July':
-            month = 7
-        elif month == 'aug' or month == 'august' or month == 'Aug' or month == 'August':
-            month = 8
-        elif month == 'sep' or month == 'september' or month == 'Sep' or month == 'September':
-            month = 9
-        elif month == 'oct' or month == 'october' or month == 'Oct' or month == 'October':
-            month = 10
-        elif month == 'nov' or month == 'november' or month == 'Nov' or month == 'November':
-            month = 11
-        elif month == 'dec' or month == 'december' or month == 'Dec' or month == 'December':
-            month = 12
-    else:
-        month = int(month)
-    
-    if futureMonth.isdigit() is False:
-        if futureMonth == 'jan' or futureMonth == 'january' or futureMonth == 'Jan' or futureMonth == 'January':
+        if futureMonth == i:
             futureMonth = 1
-        elif futureMonth == 'feb' or futureMonth == 'february' or futureMonth == 'Feb' or futureMonth == 'February':
+
+    for i in feb:
+        if month == i:
+            month = 2
+        if futureMonth == i:
             futureMonth = 2
-        elif futureMonth == 'mar' or futureMonth == 'march' or futureMonth == 'Mar' or futureMonth == 'March':
+
+    for i in mar:
+        if month == i:
+            month = 3
+        if futureMonth == i:
             futureMonth = 3
-        elif futureMonth == 'apr' or futureMonth == 'april' or futureMonth == 'Apr' or futureMonth == 'April':
+
+    for i in apr:
+        if month == i:
+            month = 4
+        if futureMonth == i:
             futureMonth = 4
-        elif futureMonth == 'may' or futureMonth == 'May':
+
+    for i in may:
+        if month == i:
+            month = 5
+        if futureMonth == i:
             futureMonth = 5
-        elif futureMonth == 'jun' or futureMonth == 'june' or futureMonth == 'Jun' or futureMonth == 'February':
+
+    for i in jun:
+        if month == i:
+            month = 6
+        if futureMonth == i:
             futureMonth = 6
-        elif futureMonth == 'jul' or futureMonth == 'july' or futureMonth == 'Jul' or futureMonth == 'July':
+
+    for i in jul:
+        if month == i:
+            month = 7
+        if futureMonth == i:
             futureMonth = 7
-        elif futureMonth == 'aug' or futureMonth == 'august' or futureMonth == 'Aug' or futureMonth == 'August':
+
+    for i in aug:
+        if month == i:
+            month = 8
+        if futureMonth == i:
             futureMonth = 8
-        elif futureMonth == 'sep' or futureMonth == 'september' or futureMonth == 'Sep' or futureMonth == 'September':
+
+    for i in sep:
+        if month == i:
+            month = 9
+        if futureMonth == i:
             futureMonth = 9
-        elif futureMonth == 'oct' or futureMonth == 'october' or futureMonth == 'Oct' or futureMonth == 'October':
+
+    for i in oct:
+        if month == i:
+            month = 10
+        if futureMonth == i:
             futureMonth = 10
-        elif futureMonth == 'nov' or futureMonth == 'november' or futureMonth == 'Nov' or futureMonth == 'November':
+
+    for i in nov:
+        if month == i:
+            month = 11
+        if futureMonth == i:
             futureMonth = 11
-        elif futureMonth == 'dec' or futureMonth == 'december' or futureMonth == 'Dec' or futureMonth == 'December':
+
+    for i in dec:
+        if month == i:
+            month = 12
+        if futureMonth == i:
             futureMonth = 12
-    else:
-        futureMonth = int(futureMonth)
+
+
+    month = int(month)
+    futureMonth = int(futureMonth)
     
     age = int(futureYear) - int(year)
     day = int(day)
