a40dcf7edec5b5bb6f02133cf4a8f0b1e6e9fc34
JolantaWro <jolanta.woronowska@gmail.com>
2024-09-15 22:26:11 +0200

    refactor view csv

--- Beginning/String_Actions/view_csv.py
+++ Beginning/String_Actions/view_csv.py
@@ -1,20 +1,20 @@
 import csv
+import os
 
 sciezka_do_pliku = input("Podaj ścieżkę do pliku CSV: ")
 
-try:
-    
-    with open(sciezka_do_pliku, newline='', encoding='utf-8') as csvfile:
-        
-        csvreader = csv.reader(csvfile)
-        
-       
-        liczba_wierszy = 5  
-        for i, row in enumerate(csvreader):
-            print(row)
-            if i >= liczba_wierszy - 1:
-                break
-except FileNotFoundError:
+
+if not os.path.isfile(sciezka_do_pliku):
     print(f"Plik o ścieżce '{sciezka_do_pliku}' nie został znaleziony.")
-except Exception as e:
-    print(f"Wystąpił błąd: {e}")
+else:
+    try:
+        with open(sciezka_do_pliku, newline='', encoding='utf-8') as csvfile:
+            csvreader = csv.reader(csvfile)
+            
+            liczba_wierszy = 5 
+            for i, row in enumerate(csvreader):
+                print(row)
+                if i >= liczba_wierszy - 1:
+                    break
+    except Exception as e:
+        print(f"Wystąpił błąd: {e}")
