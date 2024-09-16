6d7bfb91e17261f04c48399d8d1b853c59ffffc8
JolantaWro <jolanta.woronowska@gmail.com>
2024-09-16 21:25:43 +0200

    init valid color function

---
+++ Beginning/String_Actions/valid_color.py
@@ -0,0 +1,22 @@
+COLOR_CODES = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+
+def color_code(color):
+    """
+    Zwraca wartość liczbową odpowiadającą danemu kolorowi.
+    
+    Argument:
+    color (str): Nazwa koloru.
+
+    Zwraca:
+    int: Wartość liczbowa dla danego koloru.
+    """
+    return COLOR_CODES.index(color)
+
+def colors():
+    """
+    Zwraca listę wszystkich dostępnych kolorów.
+
+    Zwraca:
+    list: Lista kolorów w odpowiedniej kolejności.
+    """
+    return COLOR_CODES
